5d814944dec1cf1c0eb8bdfd1276b3d210860bee
danilodequeiroz <danilo.aq@gmail.com>
2024-06-28 09:37:46 -0300

    lesson 3: learning how to use dependencyResolutionManagement and pluginManagement, and defining repositories for each one of this sectors, such mavenCentral google and self-hosted one if exists. Plus also learning how include builds, such as a dependency folder or a folder for gradle plugins.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,27 @@
+pluginManagement {
+    repositories.gradlePluginPortal()
+    repositories.google()
+    repositories.maven("http://localhost/repo"){
+        credentials.username = ""
+        credentials.password = ""
+    }
+    includeBuild("gradle/plugins")
+}
 
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+    repositories.google()
+    repositories.maven("http://localhost/repo"){
+        credentials.username = ""
+        credentials.password = ""
+    }
+
+    // includes another repository or project
+    // to build along with the current (current: gradle-from-scratch)
+    includeBuild("../gradle-from-scratch-dependant-build")
+}
+
+rootProject.name = "gradle-from-scratch"
 
 include("app")
 include("business-logic")
