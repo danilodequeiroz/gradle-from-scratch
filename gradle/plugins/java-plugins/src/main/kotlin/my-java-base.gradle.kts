a7ccdb680e596750fea786f7ba33963fa45a1936
danilodequeiroz <danilo.aq@gmail.com>
2024-07-01 22:34:20 -0300

    lesson 8:  - Configuring tasks from plugins, more specifically my-java-base.gradle.kts.    - Configuring using default accessor (tasks.compileJava {});    - By type (withType<Class>().configureEach {})    - Using named<ClassType>("compileJava") {}  notation

--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -1,3 +1,5 @@
+import gradle.kotlin.dsl.accessors._a36adeff7943c18d5e9e07d718caef3f.compileTestJava
+
 plugins {
     id("java")
     id("com.diffplug.spotless")
@@ -6,4 +8,33 @@ plugins {
 // Extension from the "java", "java-library" and "application" plugins
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(11))
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
+
+tasks.compileTestJava {
+    options.encoding = "UTF-8"
+}
+
+// Always be careful with this setup
+// In this case make sense because it's just encoding everything using utf-8,
+// but for other configurations wouldn't  be a good practice
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.named<JavaCompile>("compileJava") {} // ==  tasks.compileJava {}
+
+tasks.compileTestJava {
+    options.encoding = "UTF-8"
+}
+
+tasks.test {
+
+}
+
+tasks.javadoc {
+
 }
