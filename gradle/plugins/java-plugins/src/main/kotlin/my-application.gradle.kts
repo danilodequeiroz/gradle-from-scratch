830adc7fa3a90d60cc7bb64a049d8f77a61858d1
danilodequeiroz <danilo.aq@gmail.com>
2024-07-01 23:55:48 -0300

    lesson 9:  - Register custom task that counts Jars and write to a file inside build/gen/count.txt

--- gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
@@ -1,4 +1,23 @@
-plugins{
+import com.github.danilodequeiroz.gradlefundamentals.JarCount
+
+plugins {
     id("application")
     id("my-java-base")
+}
+
+tasks.register<JarCount>("countJars") {
+    group = "Custom Zipping"
+    description = "Counts!."
+    allJars.from(tasks.jar)
+    allJars.from(configurations.runtimeClasspath)
+    countFile.set(layout.buildDirectory.file("gen/count.txt"))
+}
+
+tasks.register<Zip>("bundle") {
+    group = "Custom Zipping"
+    description = "Zipping it all!."
+    from(tasks.jar)
+    from(configurations.runtimeClasspath)
+    destinationDirectory.set(layout.buildDirectory.dir("distribution"))
+//    from("build/libs/app.jar")
 }
