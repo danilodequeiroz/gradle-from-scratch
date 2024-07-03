c07d3ac6f2a204cf96ea36077b4dc4aad4a9eb7a
danilodequeiroz <danilo.aq@gmail.com>
2024-07-03 02:08:50 -0300

    lesson 12:  - demonstrating runtimeOnly scope;  - demonstrating compileOnly scope;

--- business-logic/build.gradle.kts
+++ business-logic/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(dependencyNotation = project(":data-model"))
     // coordinate notation : (group : name : version) 
     implementation(dependencyNotation = "org.apache.commons:commons-lang3:3.12.0")
+//    api(dependencyNotation = "org.apache.commons:commons-lang3:3.12.0")  // <- if you wish to expose all classes and references to upper level (app module)
     implementation("org.slf4j:slf4j-api:1.7.36")
 
 //    api("") // <-- dependency is transitively visible as compilatio (java-library only)
