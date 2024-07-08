c6276798a61412e4fa2a3448a0db64ea15cf743b
Danilo de Queiroz <danilo.aq@gmail.com>
2024-07-08 00:03:10 -0300

    lesson 12: Changes dependency on app modules/component build.gradle.kts, from slf4j-api to slf4j-simple;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,6 @@ application {
 dependencies {
     implementation(dependencyNotation = project(":business-logic"))
     implementation(dependencyNotation = project(":data-model"))
-    runtimeOnly("org.slf4j:slf4j-api:2.0.13")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.13")
 
 }
